0fb49db6766bf7b7c7e50e89123aea946c228692
Saad Arif <arifsaad@gmail.com>
2023-06-19 12:38:00 +0100

    Add bash script for PCA with angsd

---
+++ bash_scripts/PCA_angsd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+#Takes ins a list of bam file sn then
+#1. converts to genotype likelihood in Beagle format
+#2. PCAangsd makes a covairance matrix from likelihoods that can be used for PCA
+
+
+############################
+#PATHS
+ANGSD=/opt/angsd/angsd
+PCANGSD=/opt/pcangsd/pcangsd/pcangsd.py
+###########################
+
+INBAMS=$1 # command line arguement list of full paths to all BAMS
+
+#Step 1
+$ANGSD -GL 2 -out genolike -nThreads 10 -doGlf 2 -doMajorMinor 1 -SNP_pval 1e-6 -uniqueOnly 1 -doMaf 2 -minMapQ 30 -minQ 20 -skipTriallelic 1 -minMaf 0.1 -doDepth 1 -doCounts 1 -bam $INBAMS
+
+#step 2
+python3 $PCANGSD --minMaf 0.1 -beagle genolike.beagle.gz -o angsdPCA
